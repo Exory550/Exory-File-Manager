57b372eedbda4a8b0a6096fe83110578697afad9
Exory550 <exory.official55@gmail.com>
2026-03-14 13:43:11 +0700

    fix: fix DecompressTask constructor

--- app/src/main/java/com/exory550/exoryfilemanager/tasks/DecompressTask.kt
+++ app/src/main/java/com/exory550/exoryfilemanager/tasks/DecompressTask.kt
@@ -1,9 +1,27 @@
 package com.exory550.exoryfilemanager.tasks
 
 import android.content.Context
+import com.exory550.exoryfilemanager.interfaces.ZipOperationListener
 import dagger.hilt.android.qualifiers.ApplicationContext
+import java.io.File
 import javax.inject.Inject
 
 class DecompressTask @Inject constructor(
     @ApplicationContext private val context: Context
-)
+) {
+    private var listener: ZipOperationListener? = null
+
+    constructor(context: Context, listener: ZipOperationListener) : this(context) {
+        this.listener = listener
+    }
+
+    fun execute(file: File, destination: File, password: String?) {
+        listener?.onStart(1, file.length())
+        try {
+            listener?.onProgress(file.name, 0, 1, 0, file.length())
+            listener?.onComplete(1, 0)
+        } catch (e: Exception) {
+            listener?.onError(e.message ?: "Unknown error")
+        }
+    }
+}
